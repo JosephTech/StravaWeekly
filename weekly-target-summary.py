2a054059564092b72ab1200f843489b9728d6621
Julian Joseph <julian@josephtechnology.net>
2018-05-08 22:05:55 +0100

    Update weekly-target-summary.py

--- weekly-target-summary.py
+++ weekly-target-summary.py
@@ -58,24 +58,32 @@ def send_summary():
     consumer = Consumer()
 
 
+    currentYear = 2018
     stravaClient = Client()
     stravaClient.access_token = stravaUser.StravaAccessToken
     athlete = stravaClient.get_athlete()
 
-    year_total = athlete.stats.ytd_ride_totals.distance.num/1000
+    #year_target = 10000
+    year_to_date_distance = athlete.stats.ytd_ride_totals.distance.num/1000
 
-    end_of_year = date(2017, 12, 31)
+    #end_of_year = date(currentYear, 12, 31)
+    start_of_year = date(currentYear, 1, 1)
+
     current_date = date.today()
-    remaining_days = (end_of_year - current_date).days
-    remaining_distance = 10000 - year_total
-    daily_target = remaining_distance/remaining_days
-    weekly_target = remaining_distance/(remaining_days/7)
+    days_to_date = (current_date - start_of_year).days
+    average_daily_distance = year_to_date_distance/days_to_date
+
+    #remaining_days = (end_of_year - current_date).days
+    projected_distance_for_year = 365 * average_daily_distance
 
 
+    #remaining_distance = year_target - year_to_date_distance
+    #daily_target = remaining_distance/remaining_days
+    #weekly_target = remaining_distance/(remaining_days/7)
 
-    status = '{0} #Strava ytd/goal: {1:.0f}/10000 - daily target: {2:.2f}km, weekly target: {3:.2f}km'.format(stravaUser.TwitterAccount, year_total, daily_target, weekly_target)
+    #status = '{0} #Strava ytd/goal: {1:.0f}/10000 - daily target: {2:.2f}km, weekly target: {3:.2f}km'.format(stravaUser.TwitterAccount, year_to_date_distance, daily_target, weekly_target)
+    status = '{0} #Strava projected distance for {1}: {2:,.2f} km based on current average of {3:.2f} km/day'.format(stravaUser.TwitterAccount, currentYear, projected_distance_for_year, average_daily_distance)
 
-    print(status)
     # create bot - veckostat twitter app
     #consumer key/consumer secret
     auth = tweepy.OAuthHandler(consumer.client_code, consumer.client_secret)
@@ -89,11 +97,11 @@ def send_summary():
 def monday_test():
     current_day = datetime.today()
     weekday = current_day.weekday()
-    if (weekday == 0):
-        print("It's Monday - woohoo :)")
-        send_summary()
-    else:
-        print("It's not Monday :(")
+    #if (weekday == 0):
+    print("It's Monday - woohoo :)")
+    send_summary()
+    #else:
+    #    print("It's not Monday :(")
 
 if __name__ == '__main__':
     monday_test()
